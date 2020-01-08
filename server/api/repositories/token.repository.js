76bbc0f4e5d43e7ff2519cee75ef0c3d14d3ae06
Oliwia Trendowska <trendowska.oliwia@gmail.com>
2020-01-08 13:35:58 +0100

    DB connection, user authentication and create user

---
+++ server/api/repositories/token.repository.js
@@ -0,0 +1,13 @@
+const tokenModel = require("../models/token");
+
+const saveToken = function(accessToken, expireDate, user, callback) {
+    tokenModel.create({
+        accessToken: accessToken,
+        expires: expireDate,
+        userId: user._id
+    }, callback)
+}
+
+module.exports = {
+    saveToken,
+}
---
+++ server/api/repositories/user.repository.js
@@ -0,0 +1,29 @@
+const userModel = require("../models/users");
+
+const createUser = function (user, callback) {
+    userModel.create({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role,
+        email: user.email,
+        password: user.password
+    }, callback)
+};
+
+const getUserByEmail = function (email, callback) {
+    userModel.findOne({
+        email: email.toLowerCase()
+    }, callback)
+};
+
+const deleteUser = function (userId, callback) {
+    userModel.findOneAndDelete({
+        _id: userId
+    }, callback)
+}
+
+module.exports = {
+    createUser,
+    getUserByEmail,
+    deleteUser
+}
